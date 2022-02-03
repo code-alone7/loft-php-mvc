c0e901c9e78459d4eae0c9679d9e9c29917afbfe
Zadorin Artyom <superdeathcorpce@gmail.com>
2022-02-04 00:05:31 +0500

    basic implementation of Model

--- src/Model.php
+++ src/Model.php
@@ -2,14 +2,35 @@
 
 namespace Core;
 
+use Core\exceptions\ModelException;
+
 abstract class Model
 {
-    private array $fields;
+    private static array $fields = [];
+    private array $values;
     private string $name;
 
     public function __construct(array $data)
     {
-        
+        $keyDiff = array_diff_key(self::$fields, $data);
+        if(count($keyDiff)===0){
+            throw new ModelException('wrong fields');
+        }
+        /*for($data as $title => $value){
+            if(gettype($value) !== self::$fields[$title]){
+                return false;
+            }
+        }*/
+
+        $this->values = $data;
+    }
+
+    public function __get(string $name)
+    {
+        if(array_key_exists($name, $this->values)){
+            return $this->values[$name];
+        }
+        return null;
     }
 
     public function save()
