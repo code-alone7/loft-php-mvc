2190bb5ad8ba305106ff3ee426f7ff1fa3e1a4af
Zadorin Artyom <superdeathcorpce@gmail.com>
2022-02-13 04:19:16 +0500

    edit config.php

--- src/config.php
+++ src/config.php
@@ -1,7 +1,9 @@
 <?php
-define('ROOT_DIR', __DIR__ . DIRECTORY_SEPARATOR . '..');
-define('TEMPLATE_DIR', ROOT_DIR . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'view');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'php-mvc-db');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'root');
+const ROOT_DIR     = __DIR__ . DIRECTORY_SEPARATOR . '..';
+const TEMPLATE_DIR = ROOT_DIR . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'view';
+
+const DB_DRIVER   = 'mysql';
+const DB_HOST     = 'localhost';
+const DB_NAME     = 'php-mvc-db';
+const DB_USER     = 'root';
+const DB_PASSWORD = 'root';
